956597eae9463736ba3b6332bcee990ccdf54af4
Lau <jinxilau9@gmail.com>
2025-06-18 16:58:13 +0800

    commi

---
+++ app/Livewire/PhotographerAssignment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PhotographerAssignment extends Component
+{
+    public function render()
+    {
+        return view('livewire.photographer-assignment');
+    }
+}
--- resources/views/livewire/user/dashboard.blade.php
+++
@@ -1,6 +0,0 @@
-<x-app-layout> <!-- Applying Blade component named app-layout -->
-    <div class="container mt-4">
-        <h2>Welcome, {{ auth()->user()->name }}</h2>
-        <p>This is your user dashboard.</p>
-    </div>
-</x-app-layout>
